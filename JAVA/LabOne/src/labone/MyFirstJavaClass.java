e638a912dae7da20a5f76dc2792de7393ae6319b
TheUnknownS <prajwalmaharjan392@gmail.com>
2025-01-29 09:36:25 +0545

    first

---
+++ JAVA/LabOne/src/labone/MyFirstJavaClass.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labone;
+
+/**
+ *
+ * @author user
+ */
+public class MyFirstJavaClass {
+    // This is the main methos that executes the whole program
+    // which run at first when the java program is executed
+    public static void main(String args[]){
+        String session = "OOP on Java First Lab Session";
+        String faculty = "BCA";
+        
+        // to print/display in console
+        System.out.println(session + " " + faculty);
+    }
+}
